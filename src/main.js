1a8e5c70bdf2c60821dccb5ebc6cb89032a072a8
Jérémy Riverain <riverainjeremy@gmail.com>
2025-09-20 23:14:06 +0200

    TP 3

--- src/main.js
+++ src/main.js
@@ -17,3 +17,60 @@ function display() {
 }
 
 console.log(display())
+
+// TP 3
+
+class Person {
+  constructor(args) {
+    this.firstName = args.firstName
+    this.lastName = args.lastName
+    this.age = args.age
+  }
+
+  toString () {
+    return `${firstName} ${lastName}, ${age} years old`
+  }
+}
+
+const person = new Person({firstName: 'Jérémy', lastName: 'Riverain', age: 39})
+
+console.log(person)
+
+console.log(person.toString())
+
+class Child extends Person {
+  constructor(args) {
+    super(args)
+    this.schoolLevel = args.schoolLevel
+  }
+
+  toString () {
+    return `${super.toString()}, school level: ${this.schoolLevel}`
+  }
+}
+
+const child = new Child({firstName: 'Foo', lastName: 'Bar', age: 10, schoolLevel: 'CP'})
+
+console.log(child)
+console.log(child.toString())
+
+
+class Adult extends Person {
+
+  #hasLicense 
+
+  constructor(args) {
+    super(args)
+    this.#hasLicense = args.hasLicense
+  }
+
+  toString () {
+    return `${super.toString()}, has license: ${this.#hasLicense}`
+  }
+}
+
+const adult = new Adult({firstName: 'James', lastName: 'Bar', age: 100, hasLicense: true})
+
+console.log(adult)
+
+console.log(adult.toString())
