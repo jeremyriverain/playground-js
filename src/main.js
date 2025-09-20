1f95a06e82691b3010cbc6ce6c8e0c42026c8cdf
Jérémy Riverain <riverainjeremy@gmail.com>
2025-09-20 22:56:41 +0200

    TP 1 & 2

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// TP 1 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const firstName = "Jérémy"
 
-setupCounter(document.querySelector('#counter'))
+const lastName = "Riverain"
+
+const age = 39
+
+const hasLicense = true
+
+console.log(firstName, lastName, age, hasLicense)
+
+// TP 2
+
+function display() {
+  return `${lastName.toUpperCase()} ${firstName}`
+}
+
+console.log(display())
